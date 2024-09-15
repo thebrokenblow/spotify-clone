838dec1b417450eae029188960c225edc4d615a5
Krasov <artkrasov@gmail.com>
2024-09-15 16:51:55 +0300

    add Sidebar

---
+++ src/components/Player.jsx
@@ -0,0 +1,12 @@
+
+const Player = () => {
+  return (
+    <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
+      <div className="hidden lg:flex items-center gap-4">
+        <img src={} alt=""/>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
