affe6092c959b4e0da9ebd340e0c0e9f7a9dfdff
Krasov <artkrasov@gmail.com>
2024-09-15 21:06:38 +0300

    Add album and songs list

---
+++ src/components/Display.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import DisplayHome from "../components/DisplayHome";
+const Display = () => {
+  return (
+    <div className="w-[100%] mt-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+      <Routes>
+        <Route path="/" element={<DisplayHome />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Display;
---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,43 @@
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import { songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
+
+const DisplayHome = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayHome;
