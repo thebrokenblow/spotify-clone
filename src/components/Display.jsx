1e01eb422c3243646523209d8008a94f48c609c3
Krasov <artkrasov@gmail.com>
2024-09-15 22:34:00 +0300

    Add songs list in playlist

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import DisplayHome from "../components/DisplayHome";
+import DisplayAlbum from "./DisplayAlbum";
 const Display = () => {
   return (
     <div className="w-[100%] mt-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
     </div>
   );
