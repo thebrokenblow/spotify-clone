d97f5e350f362ca49218d8d8aa3a4406893959da
Krasov <artkrasov@gmail.com>
2024-09-15 18:17:59 +0300

    add player components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Sidebar from "./components/Sidebar";
-
+import Player from "./components/Player";
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
       </div>
+      <Player />
     </div>
   );
 };
